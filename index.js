770078e635b1b0df05fd712710c7aa76186c1e8a
Kenan Alnaser <kenan-na@live.com>
2021-02-17 15:31:17 +0100

    second branch update

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 // hackerSpeak
 // the function should replace all 'a's with 4, 'e's with 3, 'i's with 1, 'o's with 0, and 's's with 5
-const hackSpeak = (
-  original,
-  replacements = { a: "4", e: "3", i: "1", o: "0", s: "5" }
-) => {
-  let userInput = original.toLowerCase();
-  let leetSpeek = Object.entries(replacements).reduce(
-    (acc, el) => acc.replace(el[0], el[1]),
-    userInput
-  );
-  return leetSpeek;
+const hackSpeak = (str) => {
+  let leetCode = {
+    A: "4",
+    B: "b",
+    C: "c",
+    D: "d",
+    E: "3",
+    F: "f",
+    G: "g",
+    H: "h",
+    I: "1",
+    J: "j",
+    K: "k",
+    L: "l",
+    M: "m",
+    N: "n",
+    O: "0",
+    P: "p",
+    Q: "q",
+    R: "r",
+    S: "5",
+    T: "t",
+    U: "u",
+    V: "v",
+    W: "w",
+    X: "x",
+    Y: "y",
+    Z: "z",
+    " ": " ",
+  };
+
+  let translatedStr = "";
+
+  for (let i = 0; i < str.length; i++) {
+    translatedStr += leetCode[str.charAt(i).toUpperCase()];
+  }
+
+  return translatedStr;
 };
-console.log(hackSpeak("I like javaScript"));
+
+console.log(hackSpeak("I like JavaScript"));
 
 // Count Occurrences.
 // Create a function that accepts two arguments: a string and a letter. The function should count the number of occurrences of that letter in the string.
@@ -19,44 +48,46 @@ console.log(hackSpeak("I like javaScript"));
 // Example:
 
 // countOccurrences("this is a string", "i") ➞ 3
-const countOccurrences = (str, char) => {
-  return [...str].filter((x) => x === char).length;
-};
+countOccurrences = (str, char) => str.split(char).length - 1;
 console.log(countOccurrences("this is a string", "i"));
 
 //Scrabble
-let letters = {
-  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-  2: ["D", "G"],
-  3: ["B", "C", "M", "P"],
-  4: ["F", "H", "V", "W", "Y"],
-  5: ["K"],
-  8: ["J", "X"],
-  10: ["Q", "Z"],
+let points = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
 };
 
-const scrabbleScore = (input) => {
-  function lettersToScore(letter) {
-    for (let index in letters) {
-      if (
-        Object.keys(letters).indexOf(
-          letters[index].indexOf(letter) !== -1 ? index.toString() : "-1"
-        ) !== -1
-      ) {
-        return parseInt(index);
-      }
-    }
+const score = (word) => {
+  let userInputWord = word.toUpperCase();
+  let pointsTotal = 0;
+  for (let i = 0; i < userInputWord.length; i++) {
+    let char = userInputWord[i];
+    pointsTotal += points[char];
   }
-  sum = 0;
-  if (input === null || input === "") {
-    return 0;
-  }
-  input
-    .toUpperCase()
-    .split("")
-    .forEach(function (elem) {
-      sum += lettersToScore(elem);
-    });
-  return sum;
+  return pointsTotal;
 };
-console.log(scrabbleScore("abby"));
+console.log(score("abby"));
