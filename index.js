c5ac9f1a3db1299d2837d4d6ac75b5e46542fc7b
Kenan Alnaser <kenan-na@live.com>
2021-02-17 15:38:36 +0100

    third branch hackspeak

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // hackerSpeak
 // the function should replace all 'a's with 4, 'e's with 3, 'i's with 1, 'o's with 0, and 's's with 5
+const hackSpeak = (str) => {
+  const leet = { a: 4, b: 3, i: 1, o: 0, s: 5 };
+  const translate = (letter) =>
+    leet.hasOwnProperty(letter) ? leet[letter] : letter;
+
+  return str.toLowerCase().split("").map(translate).join("");
+};
+console.log(hackSpeak("I like JavaScript"));
 
 // Count Occurrences.
 // Create a function that accepts two arguments: a string and a letter. The function should count the number of occurrences of that letter in the string.
